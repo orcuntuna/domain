ec8664ddbd32968a45b79e548f61a4d21521f943
Orçun Tuna <turboindirblog@gmail.com>
2022-10-08 18:43:50 +0300

    update prod environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  domainApi: {
+    url: "https://domainr.p.rapidapi.com",
+    host: "domainr.p.rapidapi.com",
+    key: "7dVRX69DTomshEC7bYR7pzermrkPp15LojIjsndQFRwtwSRFMU"
+  },
+  whoisApi: {
+    url: "https://domaination.p.rapidapi.com",
+    host: "domaination.p.rapidapi.com",
+    key: "7dVRX69DTomshEC7bYR7pzermrkPp15LojIjsndQFRwtwSRFMU"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,7 +13,7 @@ export const environment = {
     url: "https://domaination.p.rapidapi.com",
     host: "domaination.p.rapidapi.com",
     key: "7dVRX69DTomshEC7bYR7pzermrkPp15LojIjsndQFRwtwSRFMU"
-  },
+  }
 };
 
 /*
